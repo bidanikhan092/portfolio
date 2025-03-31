8ba0a22df6c43f07d92fb33a2af581133b5e094e
Skotix <129529227+bidanikhan092@users.noreply.github.com>
2025-03-31 19:44:09 +0500

    Info Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Footer() {
   return (
 <div className="footer d-flex justify-content-between align-items-center p-3">
-  <div className="footer-left">📧 Email: your@email.com</div>
+  <div className="footer-left">📧 Email: skillanimationfreelancer@gmail.com</div>
   <div className="footer-center text-center">© 2025 All Rights Reserved</div>
-  <div className="footer-right">📞 +123 456 7890</div>
+  <div className="footer-right">📞 +92 310-2819000</div>
 </div>
   )
 }
